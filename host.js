d3624e0f4e0d8b55106672532f91d8cac6b89ff4
hiddendev <coquelet.c@gmail.com>
2015-11-26 18:44:32 +0100

    modify host

--- host.js
+++ host.js
@@ -3,7 +3,7 @@ var express    = require('express');
 var app        = express();
 var server = require('./server');
 var vhost = require('vhost');
-var evh = require('express-vhost');
+
 console.log(server);
 //app.use(vhost('*.example.prod', server)); // Serves all subdomains via Redirect app
 app.use(vhost('blog-creator.prod', server)); // Serves top level domain via Main server app
