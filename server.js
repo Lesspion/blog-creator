706eae0d717b7df8911b07c57cb8cff1fd1f861a
Adeline Bourgeois <abs@awakit.com>
2015-11-30 16:24:09 +0100

    Page profil header

--- server.js
+++ server.js
@@ -44,5 +44,12 @@ app.get('/blog/article', function (req, res) {
 	});
 });
 
+app.get('/profil', function (req, res) {
+	res.render('BaseBack/profil', {
+		pagename: "Profil",
+		authors: ['Adeline', 'Chris']
+	});
+});
+
 app.listen(port);
 console.log('Magic happens on port ' + port);
