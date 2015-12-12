10609f5b5db7fea7b3f7139ac006b04ba3fbca73
hiddendev <coquelet.c@gmail.com>
2015-12-12 21:17:49 +0100

    write in readme && fixed indentation in host.js && change command npm start and test from package.json

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
-Just do the following statements:
+Before starting, just assume you have the prot 80 free, after
+just do the following statements:
 -npm install
 -npm start
--- host.js
+++ host.js
@@ -1,12 +1,12 @@
-var port = 80;
-var express    = require('express');
-var app        = express();
-var server = require('./server');
-var vhost = require('vhost');
+var port	= 80;
+var express = require('express');
+var app     = express();
+var server 	= require('./server');
+var vhost	= require('vhost');
 
 app.use(vhost('blog-creator.prod', server));
 app.use(vhost('*.blog-creator.prod', server));
-//app.use(evh.vhost(app.enabled('trusted proxy')));
+
 app.listen(port);
 
 console.log('Magic happens on port ' + port);
